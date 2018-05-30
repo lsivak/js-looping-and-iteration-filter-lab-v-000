e1db2f5bdf9b36228187f07ee5889941c434d2bc
Lara Sanders <larasanders@gmail.com>
2018-05-30 18:59:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-// Code your solution in this file
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, function(driver) {return driver ==== driver})
