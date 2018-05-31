1de562b6851b5cebf34b8a3ee14d7d02e1c23895
Lara Sanders <larasanders@gmail.com>
2018-05-31 17:52:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function findMatching(collection, name) {
   return driver.toLowerCase() === name.toLowerCase()
 })
 }
-function findMatching(collection, name) {
+function fuzzyMatch(collection, name) {
   return collection.filter(function (guessDriver) {
  return guessDriver[0] === name
 })
