e9d005368abacb36203a6dee03b6f4fe3c6eedb5
Lara Sanders <larasanders@gmail.com>
2018-05-31 17:44:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 function findMatching(collection, name) {
 
-   return collection.filter(function (collectionName) {
-  return collectionName() === name
+   return collection.filter(function (driverName) {
+  return driverName() === name
 })
 }
