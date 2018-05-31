531cc6447efce09ade79d347c4e764097451ebc2
Lara Sanders <larasanders@gmail.com>
2018-05-31 16:47:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 
 function findMatching(drivers, name) {
 debugger
-  for (const name of drivers) {
+  for (const driver of drivers) {
   if (driver[name] === name) {
   console.log(driver);
 }
