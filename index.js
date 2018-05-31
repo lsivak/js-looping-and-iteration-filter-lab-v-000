b232a5254ec0f4416e0f943139fb5b05e753ea6d
Lara Sanders <larasanders@gmail.com>
2018-05-31 16:45:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 
-function findMatching(drivers, driver) {
+function findMatching(drivers, name) {
 
-  for (const name of drivers) {
-  if (driver === name) {
+  for (const driver of drivers) {
+  if (driver[name] === name) {
   console.log(driver);
 }
 }
