d9460aff63eebe85fdfe2647a9ab4eabed0c93c9
Lara Sanders <larasanders@gmail.com>
2018-05-31 17:48:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 function findMatching(collection, name) {
 
    return collection.filter(function (driver) {
-  return driver.toLowerCase === name.toLowerCase
+  return driver.toLowerCase() === name.toLowerCase()
 })
 }
